c5e4a865d531abf9f1feafa7b455694bc6cd15e6
Tim Mastny <tim.mastny@gmail.com>
2024-12-08 09:16:48 -0600

    build data progress

--- build_data.py
+++ build_data.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 import subprocess
-from parse_branches import parse_branches
 import sys
+from parse_branches import parse_branches
+from trace import trace
+
+
+def generate_commands_lldb(branches):
+    with open("commands.lldb.tmp", "r") as f:
+        template = f.read()
+
+    bps = []
+    bp_template = "breakpoint set --address {}"
+    for pc in branches:
+        bps.append(bp_template.format(pc))
+
+    with open("commands.lldb", "w") as f:
+        f.write(template.format("\n".join(bps)))
+
 
 if __name__ == "__main__":
     args = set(sys.argv)
@@ -13,3 +28,11 @@ if __name__ == "__main__":
     if "--test" in args:
         test_keys = set(["0x100007f09", "0x100006685"])
         branches = {k: v for k, v in branches.items() if k in test_keys}
+
+    generate_commands_lldb(branches)
+
+    log = subprocess.check_output(["lldb", "-b", "-s", "commands.lldb"], text=True)
+
+    output = trace(log.splitlines())
+
+    print(output)
